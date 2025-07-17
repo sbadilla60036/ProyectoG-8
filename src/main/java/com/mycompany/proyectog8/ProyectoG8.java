e53d004e58ced00c35cf536f1cf4708653754424
sbadilla60036 <sbadilla60036@ufide.ac.cr>
2025-07-16 18:16:59 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/proyectog8/ProyectoG8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectog8;
+
+/**
+ *
+ * @author sarab
+ */
+public class ProyectoG8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

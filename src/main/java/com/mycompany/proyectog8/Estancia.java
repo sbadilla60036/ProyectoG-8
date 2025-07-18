5fc9250ccf5b183474b70f24b3e88559827a32dc
Andrikrojas <andrikrojas300706@gmail.com>
2025-07-18 16:50:15 -0600

    Update Estancia.java

--- src/main/java/com/mycompany/proyectog8/Estancia.java
+++ src/main/java/com/mycompany/proyectog8/Estancia.java
@@ -9,5 +9,27 @@ package com.mycompany.proyectog8;
  * @author sarab
  */
 public class Estancia {
+        private String fechaIngreso;
+    private int numeroNoches;
+    private int numeroHabitacion;
+    private int piso;
+    int dias;
+    public Estancia(String fechaIngreso, int numeroNoches, int numeroHabitacion, int piso) {
+        this.fechaIngreso = fechaIngreso;
+        this.numeroNoches = numeroNoches;
+        this.numeroHabitacion = numeroHabitacion;
+        this.piso = piso;
+    }
+    
+    public Estancia(int piso, int numeroHabitacion){
+        this.piso = piso;
+        this.numeroHabitacion = this.numeroHabitacion;
+    }
+    public Estancia(int dias){
+        this.dias = dias;
+    }
+
+    public int getNumeroHabitacion() { return numeroHabitacion; }
+    public int getPiso() { return piso; }
     
 }
